a4bb35278e281b0588a1760f88eca176523de254
Ezran <phantom.ezran@gmail.com>
2015-05-03 23:00:42 -0400

    distance and trip modes work

--- EDLproject.ino
+++ EDLproject.ino
@@ -82,43 +82,52 @@ void loop() {
 }
 
 void tripMode() {
-  for(;;){
-    //updateGPS();
-  float lat1, lon1;
-       float lat2, lon2;
-       float totalDistance;
-       unsigned long age;
-       
-       //float lat1, lon1;
-       //gps.f_get_position(&lat1, &lon1);
-       LCD_Serial.write(12); // Clear LCD
-       delay(5);
-       LCD_Serial.print("Trip Started");
-       gps.f_get_position(&lat1, &lon1, &age);
-       delay(2000);
-       LCD_Serial.write(12); // Clear LCD
-       delay(5);
-       LCD_Serial.print("'Select' to end");
-
-      do {
-        gps.f_get_position(&lat2, &lon2);
-        totalDistance = totalDistance + TinyGPS::distance_between(lat1, lon1, lat2, lon2);
-        delay(2000);
-      } while (butSelect == LOW);
-
-      LCD_Serial.write(12); // Clear LCD
-      delay(5);
+  float totalDistance = 0;
+  float old_lat, old_lon; 
+  float new_lat, new_lon;
+    
+  while(!updateGPS())
+      Serial.println("Waiting for init gps data");
+  gps.f_get_position(&new_lat,&new_lon);
+  
+  LCD_Serial.write(12); // Clear LCD
+  delay(5);
+  LCD_Serial.print("Trip Started");
+  delay(2000);
+  LCD_Serial.write(12); // Clear LCD
+  delay(5);
+  LCD_Serial.print("'Select' to end");
+  
+  for(;;) {
+    butSelect = digitalRead(buttonSelect);  // read input value
+    while(!updateGPS()){}
+      //Serial.println("Waiting for gps data");
+      
+    old_lat = new_lat; old_lon = new_lon;
+    gps.f_get_position(&new_lat, &new_lon);
+    
+    float delta = TinyGPS::distance_between(old_lat, old_lon, new_lat, new_lon);
+    Serial.println(delta);
+    Serial.println(totalDistance);
+    totalDistance = totalDistance + delta; 
+      
+      if (butSelect == HIGH){
+        LCD_Serial.write(12); // Clear LCD
+        delay(5);
       LCD_Serial.println("Total Distance (m)");
       LCD_Serial.print(totalDistance);
-      delay(100000);
-  }
+      delay(2000);
+         while(true){/*end*/}
+      }
+   }
 }
 
 void distMode() {
-  //get first gps data here
+  while (!updateGPS())
+     Serial.println("Waiting for valid start gps data");
+  
   float lat1, lon1;
-  unsigned long age;
-  gps.f_get_position(&lat1, &lon1, &age);
+  gps.f_get_position(&lat1, &lon1);
   Serial.println(lat1);
   Serial.println(lon1);
   Serial.println("---------");
@@ -131,30 +140,28 @@ void distMode() {
      LCD_Serial.write(12); // Clear LCD
      delay(5);
      float lat2, lon2;
-     unsigned long age;
-     gps.f_get_position(&lat2, &lon2, &age);
+     
+     while(!updateGPS())
+         Serial.println("Waiting for valid end gps data point");
+     
+     gps.f_get_position(&lat2, &lon2);
      Serial.println(lat2);
      Serial.println(lon2);
-     float Distance = TinyGPS::distance_between(lat1, lon1, lat2, lon2);
      LCD_Serial.println("Total Distance (m)");
-     LCD_Serial.print(Distance); 
+     LCD_Serial.print(TinyGPS::distance_between(lat1, lon1, lat2, lon2)); 
      while(true){/*end*/};
    }
   }
 }
 
-void updateGPS() {
-  if (millis() - start > 3000) {    // Update every 3 seconds
-    if (feedgps())
-      newdata = true;
-    start = millis();
+//feed gps data if available
+bool updateGPS() {
+  while (nss.available()){
+    if (gps.encode(nss.read()))
+      return true;
   }
-
-  if (newdata)
-    gpsdump(gps);
+  return false;
 }
-///////////
-
 
 // Get and process GPS data
 void gpsdump(TinyGPS &gps) {
@@ -167,14 +174,7 @@ void gpsdump(TinyGPS &gps) {
   //  gps.f_get_position(&lat2, &lon2);
   //  (unsigned long)TinyGPS::distance_between(lat1, lon1, lat2, lon2);
 }
-//Feed data as it becomes available
-bool feedgps() {
-  while (nss.available()) {
-    if (gps.encode(nss.read()))
-      return true;
-  }
-  return false;
-}
+
 
 /*******************************************************************************
 GPS Code
